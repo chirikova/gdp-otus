c807cebdb40a42d36ba9132d8392f0338eaeb52e
Tatyana Desyatkina <tatyana.desyatkina@lenvendo>
2023-06-27 00:59:52 +0300

    HW03 completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,63 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"regexp"
+	"sort"
+	"strings"
+)
+
+func Top10(str string) []string {
+	if len(str) == 0 {
+		return nil
+	}
+
+	splitPattern := regexp.MustCompile(`[\p{P}\p{S}]*\s+`)
+
+	type wordOccurrence struct {
+		word  string
+		count int
+	}
+
+	// считаем количество вхождений каждого слова
+	wordsCount := make(map[string]int, 0)
+
+	for _, val := range splitPattern.Split(str, -1) {
+		val = strings.ToLower(val)
+
+		if len(val) == 0 {
+			continue
+		}
+
+		wordsCount[val]++
+	}
+
+	// формируем срез из слов с количеством вхождений
+	words := make([]wordOccurrence, 0)
+
+	for word, count := range wordsCount {
+		words = append(words, wordOccurrence{word, count})
+	}
+
+	// сортируем по убыванию и лексикографически
+	sort.Slice(words, func(i, j int) bool {
+		if words[i].count == words[j].count {
+			return strings.Compare(words[i].word, words[j].word) <= 0
+		}
+
+		return words[i].count > words[j].count
+	})
+
+	// формируем первые топ 10 или меньше
+	topCount := 10
+	if len(words) < 10 {
+		topCount = len(words)
+	}
+
+	result := make([]string, 0)
+
+	for _, item := range words[0:topCount] {
+		result = append(result, item.word)
+	}
+
+	return result
 }
--- hw03_frequency_analysis/top_test.go
+++ hw03_frequency_analysis/top_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 // Change to true if needed.
-var taskWithAsteriskIsCompleted = false
+var taskWithAsteriskIsCompleted = true
 
 var text = `Как видите, он  спускается  по  лестнице  вслед  за  своим
 	другом   Кристофером   Робином,   головой   вниз,  пересчитывая
@@ -78,5 +78,9 @@ func TestTop10(t *testing.T) {
 			}
 			require.Equal(t, expected, Top10(text))
 		}
+	})
+
+	t.Run("string has less than 10 word occurrences", func(t *testing.T) {
+		require.Less(t, len(Top10("два три четыре два один")), 10)
 	})
 }
