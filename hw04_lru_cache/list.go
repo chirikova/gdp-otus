7bb9e8f20417d3027c100af6f80a9f84a321bd80
Tatyana Desyatkina <tatyana.desyatkina@lenvendo>
2023-07-02 13:42:39 +0300

    HW04 list wip

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -17,8 +17,112 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	len   int
+	start *ListItem
+	end   *ListItem
+}
+
+func (l list) Len() int {
+	return l.len
+}
+
+func (l *list) Front() *ListItem {
+	return l.start
+}
+
+func (l *list) Back() *ListItem {
+	return l.end
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	newItem := ListItem{v, nil, nil}
+
+	if l.start != nil {
+		l.start.Prev, newItem.Next = &newItem, l.start
+	}
+
+	if l.start == nil && l.end != nil {
+		l.end.Prev = &newItem
+		newItem.Next = l.end
+	}
+
+	l.start = &newItem
+
+	l.len++
+
+	return l.Front()
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	newItem := ListItem{v, nil, nil}
+
+	if l.end != nil {
+		l.end.Next, newItem.Prev = &newItem, l.end
+	}
+
+	if l.start != nil && l.end == nil {
+		l.start.Next = &newItem
+		newItem.Prev = l.start
+	}
+
+	l.end = &newItem
+
+	l.len++
+
+	return l.Back()
+}
+
+func (l *list) Remove(i *ListItem) {
+	l.len--
+
+	// проверяем кейс если удаляется стартовый элемент и у него есть следующий элемент
+	if i == l.start && i.Next != nil {
+		i.Next.Prev = nil
+		l.start = i.Next
+		return
+	}
+
+	// проверяем кейс если удаляется стартовый элемент без следующего элемента
+	if i == l.start {
+		l.start = nil
+		return
+	}
+
+	// проверяем кейс если удаляется конечный элемент и у него есть предыдущий элемент
+	if i == l.end && i.Prev != nil {
+		i.Prev.Next = nil
+		l.end = i.Prev
+		return
+	}
+
+	// проверяем кейс если удаляется конечный элемент без предыдущего элемента
+	if i == l.end {
+		l.end = nil
+		return
+	}
+
+	// кейс если удаляется не стартовый и не конечный элемент
+	i.Prev.Next, i.Next.Prev = i.Next, i.Prev
+}
+
+func (l *list) MoveToFront(i *ListItem) {
+	if i == l.start {
+		return
+	}
+
+	if i == l.end {
+		i.Prev.Next = nil
+		l.end = i.Prev
+	} else {
+		i.Prev.Next, i.Next.Prev = i.Next, i.Prev
+	}
+
+	i.Prev = nil
+	i.Next = l.start
+
+	l.start = i
+
+	return
 }
 
 func NewList() List {
